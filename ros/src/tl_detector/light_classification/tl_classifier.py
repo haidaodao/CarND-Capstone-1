89e58206a0d4802de1e171c13980dbf1e45f537e
thomasdunlap <tom.a.dunlap@gmail.com>
2018-12-27 22:36:55 -0800

    feat: updated traffic light classifier.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
@@ -15,5 +17,33 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        light = TrafficLight.UNKNOWN
+        # HSV allows count color within hue range
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        RED_MIN1 = np.array([0, 100, 100], np.unit8)
+        RED_MAX1 = np.array([10, 255, 255], np.uint8)
+
+        RED_MIN2 = np.array([160, 100, 100], np.unit8)
+        RED_MAX2 = np.array([179, 255, 255], np.uint8)
+
+        red_mask1 = cv2.inRange(hsv_img, RED_MIN1, RED_MAX1)
+        red_mask2 = cv2.inRange(hsv_img, RED_MIN2, RED_MAX2)
+        if cv2.countNonZero(red_mask1) + cvcv2.countNonZero(red_mask1) > 50:
+            light = TrafficLight.RED
+
+        YELLOW_MIN = np.array([40.0/360*255, 100, 100], np.unit8)
+        YELLOW_MAX = np.array([66.0/360*255, 255, 255], np.uint8)
+
+        yellow_mask = cv2.inRange(hsv_img, YELLOW_MIN, YELLOW_MAX)
+        if cv2.countNonZero(yellow_mask) > 50:
+            light = TrafficLight.YELLOW
+
+        GREEN_MIN = np.array([40.0/360*255, 100, 100], np.unit8)
+        GREEN_MAX = np.array([66.0/360*255, 255, 255], np.uint8)
+
+        green_mask = cv2.inRange(hsv_img, GREEN_MIN, GREEN_MAX)
+        if cv2.countNonZero(green_mask) > 50:
+            light = TrafficLight.GREEN
+
+        return light
